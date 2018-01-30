2cf5ee0743d7ff03d6f8f20686ca09c564fbd17c
Bruno Willenborg <b.willenborg@tum.de>
2018-01-30 01:56:16 +0100

    minor fixes

--- 3dcitydb.sh
+++ 3dcitydb.sh
@@ -14,7 +14,7 @@ if [ -z ${POSTGRES_USER+x} ] || [ "$POSTGRES_USER" = "postgres" ] ; then
   echo "   Consider changing the default username for security reasons!"
   echo
   echo "   To change the default POSTGRES_USER, use the docker run \"-e\" switch."
-  echo "   Example: \"docker run -d -e \"POSTGRES_USER=newuser\" 3dcitydb\""
+  echo "   Example: \"docker run -e \"POSTGRES_USER=newuser\" tumgis/3dcitydb-postgis\""
   echo "################################################################################"
 fi
 
@@ -26,7 +26,7 @@ if [ "$POSTGRES_PASSWORD" = "postgres" ] ; then
   echo "   Consider changing the default password for security reasons!"
   echo
   echo "   To change the default POSTGRES_PASSWORD, use the docker run \"-e\" switch."
-  echo "   Example: \"docker run -d -e \"POSTGRES_PASSWORD=newuser\" 3dcitydb\""
+  echo "   Example: \"docker run -e \"POSTGRES_PASSWORD=newuser\" 3dcitydb\""
   echo "################################################################################"
 fi
 
@@ -36,13 +36,13 @@ export PGPASSWORD="$POSTGRES_PASSWORD"
 
 # Set default env: POSTGRES_PASSWORD, CITYDBNAME, SRID, SRSNAME
 echo
-echo "# Setting up 3DCityDB environment ... ##########################################"
+echo "# Setting up tumgis/3dcitydb-postgis environment ... ###########################"
 if [ -z ${CITYDBNAME+x} ]; then
   export CITYDBNAME="3dcitydb-docker";
   echo "NOTE:"
   echo "   CITYDBNAME has not been set. Using default CITYDBNAME=${CITYDBNAME}."
   echo "   To change the default CITYDBNAME, use the docker run \"-e\" switch."
-  echo "   Example: \"docker run -d -e \"CITYDBNAME=myCustomDatabaseName\" 3dcitydb\""
+  echo "   Example: \"docker run -e \"CITYDBNAME=myCustomDatabaseName\" tumgis/3dcitydb-postgis\""
 fi
 
 if [ -z ${SRID+x} ]; then
@@ -51,7 +51,7 @@ if [ -z ${SRID+x} ]; then
   echo "NOTE:"
   echo "   SRID has not been set. Using default SRID=${SRID}."
   echo "   To change the default SRID, use the docker run \"-e\" switch."
-  echo "   Example: \"docker run -d -e \"SRID=31468\" 3dcitydb\""
+  echo "   Example: \"docker run -e \"SRID=31468\" tumgis/3dcitydb-postgis\""
 fi
 
 if [ -z ${SRSNAME+x} ]; then
@@ -61,7 +61,7 @@ if [ -z ${SRSNAME+x} ]; then
   echo "NOTE:"
   echo "   SRSNAME has not been set. Using default SRSNAME=\"${SRSNAME}\"."
   echo "   To change the default SRSNAME, use the docker run \"-e\" switch."
-  echo "   Example: \"docker run -d -e \"SRSNAME=urn:adv:crs:DE_DHDN_3GK4*DE_DHN92_NH\" 3dcitydb\""
+  echo "   Example: \"docker run -e \"SRSNAME=urn:adv:crs:DE_DHDN_3GK4*DE_DHN92_NH\" tumgis/3dcitydb-postgis\""
 fi
 
 echo
@@ -70,23 +70,24 @@ echo "# Setting up 3DCityDB environment ...done! ###############################
 # Create database
 echo
 echo "# Setting up 3DCityDB ... ######################################################"
+echo
 echo "Creating database $CITYDBNAME ..."
 echo "CREATE DATABASE \"$CITYDBNAME\";" | "${psql[@]}"
 echo "Creating database $CITYDBNAME ...done!"
-echo
 
 # Setup PostGIS extension
+echo
 echo "Create PostGIS extensions in database $CITYDBNAME ..."
-"${psql[@]}" --dbname="$CITYDBNAME" <<-'EOSQL'
+"${psql[@]}" -d "$CITYDBNAME" <<-'EOSQL'
     CREATE EXTENSION IF NOT EXISTS postgis;
 EOSQL
 echo "Create PostGIS extensions in database $CITYDBNAME ...done!"
-echo
 
 # setup 3dcitydb
+echo
 echo "Setting up 3DcityDB version $CITYDBVERSION database schema in $CITYDBNAME ..."
 cd /3dcitydb
-"${psql[@]}" -d "$CITYDBNAME" -U "$POSTGRES_USER" -f "../3dcitydb/CREATE_DB.sql" -v srsno="$SRID" -v srsname="$SRSNAME" > /dev/null
+"${psql[@]}" -d "$CITYDBNAME" -f "../3dcitydb/CREATE_DB.sql" -v srsno="$SRID" -v srsname="$SRSNAME" > /dev/null
 echo "Setting up 3DcityDB version $CITYDBVERSION database schema in $CITYDBNAME ...done!"
 echo
 echo "# Setting up 3DCityDB ... done! ################################################"
@@ -95,29 +96,28 @@ echo "# Setting up 3DCityDB ... done! ##########################################
 cat <<EOF
 
 
-# 3DCityDB Docker #############################################################
+# 3DCityDB Docker PostGIS ######################################################
 #
-# PostgreSQL/PostGIS ----------------------------------------------------------
+# PostgreSQL/PostGIS -----------------------------------------------------------
 #   PostgreSQL version  $PG_MAJOR - $PG_VERSION
 #   PostGIS version     $POSTGIS_MAJOR - $POSTGIS_VERSION
 #     PG User           $POSTGRES_USER
 #     PG Password       $POSTGRES_PASSWORD
 #
-# 3DCityDB --------------------------------------------------------------------
+# 3DCityDB ---------------------------------------------------------------------
 #   3DCityDB version  $CITYDBVERSION
 #     version info    https://github.com/3dcitydb/3dcitydb/releases/tag/v$CITYDBVERSION
 #   DBNAME            $CITYDBNAME
 #   SRID              $SRID
 #   SRSNAME           $SRSNAME
 #
-# Maintainer ------------------------------------------------------------------
+# Maintainer -------------------------------------------------------------------
 #   Bruno Willenborg
 #   Chair of Geoinformatics
 #   Department of Civil, Geo and Environmental Engineering
 #   Technical University of Munich (TUM)
 #   <b.willenborg@tum.de>
 #
-###############################################################################
-
+################################################################################
 
 EOF
