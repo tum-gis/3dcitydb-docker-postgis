f19d8a00afe71ee79822ba1175804543cfe3a5ac
Bruno Willenborg <b.willenborg@tum.de>
2019-08-06 17:44:34 +0200

    improved setup-hooks script

--- hooks/setup-hooks.sh
+++ hooks/setup-hooks.sh
@@ -1,19 +1,34 @@
-#!/bin/bash
-# run this to setup hooks for this repo
+#!/usr/bin/env bash
+
+# setup hooks located in current directory for a git repo ---------------------
+
+# git hooks
+hooks=('applypatch-msg' 'pre-applypatch' 'post-applypatch' 'pre-commit' \
+	'prepare-commit-msg' 'commit-msg' 'post-commit' 'pre-rebase' 'post-checkout' \
+	'post-merge' 'pre-push' 'pre-receive' 'update' 'post-receive' 'post-update' \
+	'pre-auto-gc' 'post-rewrite')
+
+# manage working dir
+pwdBefore=$PWD
+REPOROOT="$(git rev-parse --show-toplevel)"
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo
-if [ -d "../.git" ]; then
-  echo "Setting up hooks..."
-  echo
-  chmod -v o+x *.sh
-  echo
-  ln -sfv ../../hooks/pre-commit ../.git/hooks/
-  echo
-  echo "Setting up hooks...done!"  
+
+if [ -d "$REPOROOT/.git" ]; then
+	echo 'Setting up hooks...'
+	echo
+	for i in "${hooks[@]}"
+	do
+    if [ -f "$i" ]; then
+			printf "\t$i\t"
+			ln -sfv "$PWD/$i" "$REPOROOT/.git/hooks"
+		fi
+	done
+	echo
+	echo 'Setting up hooks...done!'
 else
-  echo This seems not to be a git repo! Doing nothing.
+	echo "$REPOROOT seems not to be a git repo, Doing nothing!"
 fi
 
-echo
-echo "Press ENTER to quit."
-read
+cd $pwdBefore
